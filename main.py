4bc2509f9230f68178699047906a39289a47ed4d
Vitaly K <vitaly.kolchenko@gmail.com>
2018-10-29 15:32:06 +0500

    Init commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+@app.route("/name")
+async def test(request,name):
+    return json({"hello": name})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
